3dcab049ffdea125db20113bde3ecadf1cffe109
Evgeni2306 <Evgeni2306@outlook.com>
2022-05-25 23:50:23 +0500

    123

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.util.*;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button RegistrationFormButton = findViewById(R.id.RegistrationFormButton);
+        EditText EmailField = findViewById(R.id.RegistrationEmail);
+        EditText NameField = findViewById(R.id.RegistrationName);
+        EditText SurnameField = findViewById(R.id.RegistrationSurname);
+        EditText PasswordField =findViewById(R.id.RegistrationPassword);
+
+        RegistrationFormButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            System.out.println(EmailField.getText());
+            }
+        });
     }
 }
