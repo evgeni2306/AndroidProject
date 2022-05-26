cc8f73ea07e99081bf54b00ed6baae05ae65a6c6
Evgeni2306 <Evgeni2306@outlook.com>
2022-05-26 14:37:41 +0500

    8

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,7 +8,8 @@ import android.view.ViewGroup;
 import android.util.*;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.database.sqlite.SQLiteDatabase;
+import android.database.Cursor;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,16 +17,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS users (email TEXT,name TEXT,surname TEXT, password TEXT)");
+//        db.execSQL("DROP Table users");
+
+
         Button RegistrationFormButton = findViewById(R.id.RegistrationFormButton);
         EditText EmailField = findViewById(R.id.RegistrationEmail);
         EditText NameField = findViewById(R.id.RegistrationName);
         EditText SurnameField = findViewById(R.id.RegistrationSurname);
         EditText PasswordField =findViewById(R.id.RegistrationPassword);
 
+
         RegistrationFormButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            System.out.println(EmailField.getText());
+            String str = "abc";
+//                db.execSQL("INSERT OR IGNORE INTO users VALUES (" +
+//                        "'" +EmailField.getText().toString()+"'" + ","+
+//                        "'" +NameField.getText().toString()+"'" + ","+
+//                        "'" +SurnameField.getText().toString() +"'" +","+
+//                        "'" +PasswordField.getText().toString()+"'" +
+//                                ")");
+//                Cursor query = db.rawQuery("SELECT * FROM users;", null);
+//                if(query.moveToFirst()){
+//
+//                    String name = query.getString(0);
+//                    System.out.println(name);
+//                }
+
             }
         });
     }
