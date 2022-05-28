2f3c78e20b0e25dfdc6d1e445a7c6940c6fcd040
Evgeni2306 <Evgeni2306@outlook.com>
2022-05-28 19:46:28 +0500

    Страница регистрации

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.view.ViewGroup;
 import android.util.*;
@@ -10,6 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.Cursor;
+import android.content.Context;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,33 +28,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS users (email TEXT,name TEXT,surname TEXT, password TEXT)");
-//        db.execSQL("DROP Table users");
+        db.execSQL("CREATE TABLE IF NOT EXISTS users (id INTEGER UNIQUE  ,email TEXT,name TEXT,surname TEXT, password TEXT," +
+                "PRIMARY KEY(\"id\" AUTOINCREMENT))");
+        db.execSQL("CREATE TABLE IF NOT EXISTS chats (id INTEGER UNIQUE,   PRIMARY KEY(\"id\"  AUTOINCREMENT))");
+        db.execSQL("CREATE TABLE IF NOT EXISTS messages (id INTEGER UNIQUE,creatorid INTEGER, chatid INTEGER, text TEXT,FOREIGN KEY(creatorid) REFERENCES users(id),FOREIGN KEY(chatid) REFERENCES chats(id),   PRIMARY KEY(\"id\"  AUTOINCREMENT))");
+        db.execSQL("CREATE TABLE IF NOT EXISTS usersinchats (id INTEGER UNIQUE, userid INTEGER, chatid INTEGER,  FOREIGN KEY(userid) REFERENCES users(id),FOREIGN KEY(chatid) REFERENCES chats(id),   PRIMARY KEY(\"id\"  AUTOINCREMENT))");
+//        db.execSQL("DROP TABLE users");
+//        db.execSQL("DROP TABLE messages");
+//        db.execSQL("DROP TABLE chats");
+//        db.execSQL("DROP TABLE usersinchats");
 
 
         Button RegistrationFormButton = findViewById(R.id.RegistrationFormButton);
         EditText EmailField = findViewById(R.id.RegistrationEmail);
         EditText NameField = findViewById(R.id.RegistrationName);
         EditText SurnameField = findViewById(R.id.RegistrationSurname);
-        EditText PasswordField =findViewById(R.id.RegistrationPassword);
+        EditText PasswordField = findViewById(R.id.RegistrationPassword);
+        TextView MessageField = findViewById(R.id.RegistrationMessage);
 
 
         RegistrationFormButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            String str = "abc";
-//                db.execSQL("INSERT OR IGNORE INTO users VALUES (" +
-//                        "'" +EmailField.getText().toString()+"'" + ","+
-//                        "'" +NameField.getText().toString()+"'" + ","+
-//                        "'" +SurnameField.getText().toString() +"'" +","+
-//                        "'" +PasswordField.getText().toString()+"'" +
-//                                ")");
-//                Cursor query = db.rawQuery("SELECT * FROM users;", null);
-//                if(query.moveToFirst()){
+                if (EmailField.getText().length()>0 && NameField.getText().length()>0 && SurnameField.getText().length()>0 && PasswordField.getText().length()>0) {
+                    db.execSQL("INSERT OR IGNORE INTO users VALUES (" +
+                            "4 ," +
+                            "'" + EmailField.getText().toString() + "'" + "," +
+                            "'" + NameField.getText().toString() + "'" + "," +
+                            "'" + SurnameField.getText().toString() + "'" + "," +
+                            "'" + PasswordField.getText().toString() + "'" +
+                            ")");
+                    Cursor query = db.rawQuery("SELECT * FROM users;", null);
+//                    if (query.moveToFirst()) {
 //
-//                    String name = query.getString(0);
-//                    System.out.println(name);
-//                }
+//                        String name = query.getString(1);
+//                        System.out.println(name);
+//                    }
+                } else {
+//                    System.out.println("asd");
+                    MessageField.setText("Заполните все поля");
+                }
+
+
 
             }
         });
